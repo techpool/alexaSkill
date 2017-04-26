b3784fbd356ec9bf37e1225956ed85e20d64d04f
techpool <suryadeep10@gmail.com>
2017-04-26 22:02:49 +0530

    rest db connected

--- alexaapps/vision/index.js
+++ alexaapps/vision/index.js
@@ -1,11 +1,28 @@
+var request = require("request");
+var REST_DB_APIKEY = '85d572967adac78254a7f9cb0b42003caaf3b';
+
 module.exports = function(app) {
 
-    app.intent("testintent", {
-            "utterances": ["say the number"]
+    app.intent("visionintent", {
+            "utterances": ["What is there in front of me"]
         },
-        function(request, response) {
-        	console.log(request)
-            response.say("All hail the number 19!");
+        function(req, res) {
+            var options = {
+                method: 'GET',
+                url: 'https://alexaskill-c6ca.restdb.io/rest/visionapi/',
+                headers: {
+                    'cache-control': 'no-cache',
+                    'x-apikey': REST_DB_APIKEY,
+                    'content-type': 'application/json'
+                },
+                json: true
+            };
+
+            request(options, function(error, response, body) {
+                if (error) throw new Error(error);
+
+                res.say(body.message);
+            });
         }
     );
 }
